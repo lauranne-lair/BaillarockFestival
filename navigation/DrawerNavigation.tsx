49edc5a23d41756259c826485147df3233cce3e1
cbjlt <c.bujault@gmail.com>
2025-01-28 12:54:43 +0100

    Drawer OK

--- navigation/DrawerNavigation.tsx
+++ navigation/DrawerNavigation.tsx
@@ -4,12 +4,18 @@ import COLORS from '../constants/colors';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import BottomTabNavigation from './BottomTabNavigation';
 
+// Import des écrans
+import Merch from '../screens/Merch';
+import Prevention from '../screens/Prevention';
+import Partenaire from '../screens/Partenaire';
+
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigation = () => {
   return (
     <Drawer.Navigator
       screenOptions={{
+        drawerPosition: 'right',
         drawerStyle: {
           backgroundColor: COLORS.darkGray,
           width: 250,
@@ -17,7 +23,7 @@ const DrawerNavigation = () => {
         headerStyle: {
           backgroundColor: COLORS.black,
         },
-        headerShown: false,
+        headerShown: false, // Masquer l'en-tête du Drawer
         headerTintColor: COLORS.white,
         drawerLabelStyle: {
           color: COLORS.white,
@@ -26,14 +32,51 @@ const DrawerNavigation = () => {
         },
       }}
     >
+      {/* Accueil */}
       <Drawer.Screen
         name="Accueil"
-        component={BottomTabNavigation} 
+        component={BottomTabNavigation}
         options={{
           drawerLabel: 'Accueil',
           title: 'Accueil',
           drawerIcon: ({ color }) => (
             <Ionicons name="home-outline" size={24} color={COLORS.white} />
+          ),
+        }}
+      />
+
+      {/* Boutique Merch */}
+      <Drawer.Screen
+        name="Merch"
+        component={Merch}
+        options={{
+          drawerLabel: 'Boutique Merch',
+          drawerIcon: ({ color }) => (
+            <Ionicons name="cart-outline" size={24} color={COLORS.white} />
+          ),
+        }}
+      />
+
+      {/* Prévention */}
+      <Drawer.Screen
+        name="Prevention"
+        component={Prevention}
+        options={{
+          drawerLabel: 'Prévention',
+          drawerIcon: ({ color }) => (
+            <Ionicons name="shield-checkmark-outline" size={24} color={COLORS.white} />
+          ),
+        }}
+      />
+
+      {/* Partenaires */}
+      <Drawer.Screen
+        name="Partenaire"
+        component={Partenaire}
+        options={{
+          drawerLabel: 'Partenaires',
+          drawerIcon: ({ color }) => (
+            <Ionicons name="people-outline" size={24} color={COLORS.white} />
           ),
         }}
       />
