ae81660e8d7515ebf16c76458a58c64fff16391d
Lauranne <lauranne.lair@gmail.com>
2025-02-05 14:55:47 +0100

    test video running order

--- styles/Programmation_Styles.ts
+++ styles/Programmation_Styles.ts
@@ -22,6 +22,10 @@ export default StyleSheet.create({
     listContent: {
       paddingBottom: height * 0.02,
     },
+    pastGroup: {
+      opacity: 0.5,
+      backgroundColor: Colors.darkGray,
+    },
     normalGroup: {
       flexDirection: 'row',
       alignItems: 'center',
@@ -31,9 +35,46 @@ export default StyleSheet.create({
       marginHorizontal: width * 0.03,
       borderRadius: 10,
     },
-    pastGroup: {
-      opacity: 0.5,
-      backgroundColor: Colors.darkGray,
+    futureGroup: {
+      opacity: 0.8,
+      backgroundColor: Colors.lightGray,
+    },
+    videoContainer: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      zIndex: 1,
+    },
+    videoBackground: {
+      width: '100%',
+      height: '100%',
+      opacity: 0.8,
+    },
+    groupImage: {
+      width: width * 0.2,
+      height: width * 0.2,
+      borderRadius: 10,
+      marginRight: width * 0.03,
+    },
+    groupDetails: {
+      flex: 1,
+    },
+    groupName: {
+      fontSize: height * 0.02,
+      fontWeight: 'bold',
+      color: Colors.white,
+    },
+    groupGenre: {
+      fontSize: height * 0.018,
+      color: Colors.secondary,
+    },
+    groupTime: {
+      fontSize: 16,
+      fontWeight: '500',
+      color: 'white',
+      textAlign: 'right',
     },
     currentGroup: {
       flexDirection: 'row',
@@ -48,68 +89,23 @@ export default StyleSheet.create({
       marginHorizontal: width * 0.03,
       borderRadius: 10,
     },
-    videoContainer: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      zIndex: -1,
-    },
-    videoBackground: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      opacity: 0.8,
-    },
-        
-    groupImage: {
-      width: width * 0.2,
-      height: width * 0.2,
-      borderRadius: 10,
-      marginRight: width * 0.03,
-    },
     currentGroupImage: {
       width: width * 0.3, 
       height: width * 0.3,
       borderRadius: 10,
       marginRight: width * 0.03,
     },
-    groupDetails: {
-      flex: 1,
-    },
-    groupName: {
-      fontSize: height * 0.02,
-      fontWeight: 'bold',
-      color: Colors.white,
-    },
     currentGroupText: {
       color: Colors.black,
       fontSize: height * 0.025, 
     },
-    groupGenre: {
-      fontSize: height * 0.018,
-      color: Colors.secondary,
-    },
     currentGenreText: {
       color: Colors.darkGray,
-    },
-    groupTime: {
-      fontSize: 16,
-      fontWeight: '500',
-      color: 'white',
-      textAlign: 'right',
     },
     currentTimeText: {
       color: Colors.black,
       fontWeight: 'bold',
       fontSize: 18,
       textAlign: 'right',
-    },
-    futureGroup: {
-      opacity: 0.8,
-      backgroundColor: Colors.lightGray,
     },
   });
