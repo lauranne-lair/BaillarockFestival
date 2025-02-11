c2743ad4de44f39baf4da051a8922cd2c7380014
ltoussaint <toussaintleeaa@gmail.com>
2025-02-11 14:48:22 +0100

    mise a jour parrainage

--- screens/Parrainage.tsx
+++ screens/Parrainage.tsx
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { useRouter } from 'expo-router';
-import * as Font from 'expo-font';
+import { FontAwesome5 } from '@expo/vector-icons';
 import { useNavigation, DrawerActions } from '@react-navigation/native';
+import * as Font from 'expo-font';
 import styles from '../styles/Parrainage_Styles';
 import Colors from '../constants/colors';
 import { parrainageConfig } from '../config/Config_Parrainage';
 
 export default function Parrainage() {
   const [fontLoaded, setFontLoaded] = useState(false);
-  const router = useRouter();
   const navigation = useNavigation();
 
-  // Charger les polices
   useEffect(() => {
     const loadFonts = async () => {
       await Font.loadAsync({
@@ -24,17 +22,11 @@ export default function Parrainage() {
     loadFonts();
   }, []);
 
-  // Personnalisation de l'en-tête
   useEffect(() => {
     navigation.setOptions({
       headerLeft: () => null,
       headerRight: () => (
-        <TouchableOpacity
-          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
-          style={styles.menuButton}
-        >
-          <Ionicons name="menu" size={30} color={Colors.dark.accent} />
-        </TouchableOpacity>
+        <Ionicons name="menu" size={30} color={Colors.dark.accent} onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())} style={styles.menuButton} />
       ),
       headerShown: true,
     });
@@ -47,30 +39,14 @@ export default function Parrainage() {
   return (
     <View style={styles.screenContainer}>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={styles.animationContainer}></View>
-
-        <Text style={styles.text}>
-          {parrainageConfig.messages.intro}
-        </Text>
-
-        <View style={styles.bulletPointContainer}>
-          <Text style={styles.bulletPoint}>•</Text>
-          <Text style={styles.text}>Invite tes amis à rejoindre l'application.</Text>
-        </View>
-        <View style={styles.bulletPointContainer}>
-          <Text style={styles.bulletPoint}>•</Text>
-          <Text style={styles.text}>Pour chaque ami inscrit grâce à toi, tu gagnes des récompenses.</Text>
-        </View>
-        <View style={styles.bulletPointContainer}>
-          <Text style={styles.bulletPoint}>•</Text>
-          <Text style={styles.text}>Ton ami bénéficie également d’un bonus de bienvenue.</Text>
-        </View>
-
-        {/* Bouton pour inviter un ami */}
-        <TouchableOpacity style={styles.inviteButton} onPress={() => alert(parrainageConfig.messages.inviteAlert)}>
-          <Text style={styles.inviteText}>{parrainageConfig.messages.inviteButton}</Text>
-        </TouchableOpacity>
-
+        <Text style={styles.title}>Qu'est ce que le Parrainage ?</Text>
+        <Text style={styles.text}>{parrainageConfig.messages.intro}</Text>
+        {parrainageConfig.messages.steps.map((step, index) => (
+          <View key={index} style={styles.bulletPointContainer}>
+            <FontAwesome5 name={step.icon} size={20} color={Colors.dark.accent} style={styles.bulletPoint} />
+            <Text style={styles.text}>{step.text}</Text>
+          </View>
+        ))}
         <Text style={styles.text}>{parrainageConfig.messages.outro}</Text>
       </ScrollView>
     </View>
