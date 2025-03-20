1c99c2d3a3304483163cfb386ee8665f50078d95
ltoussaint <toussaintleeaa@gmail.com>
2025-03-20 10:45:55 +0100

    changement du bar again

--- screens/Bar.tsx
+++ screens/Bar.tsx
@@ -1,41 +1,36 @@
-import React, { useState } from 'react';
-import { View, ScrollView, Image, TouchableOpacity, Linking, Dimensions } from 'react-native';
+import React from 'react';
+import { View, ScrollView, Image, TouchableOpacity, Text, Linking } from 'react-native';
 import { barConfig } from '../config/Config_BarPage';
 import styles from '../styles/Bar_Styles';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-const { width } = Dimensions.get('window');
-
 const Boisson: React.FC = () => {
-  const [imageHeight, setImageHeight] = useState(300); // Hauteur initiale par défaut
-
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={styles.imageContainer}>
-        {/* IMAGE PRINCIPALE */}
-        <Image
-          source={barConfig.assets.image}
-          style={{ width: width, height: imageHeight }}
-          resizeMode="contain" // Garde toute l'image visible sans la couper
-          onLoad={(event) => {
-            const { width: imgWidth, height: imgHeight } = event.nativeEvent.source;
-            setImageHeight((width * imgHeight) / imgWidth); // Ajuste la hauteur en fonction du ratio de l'image
-          }}
-        />
+      {/* IMAGE PRINCIPALE */}
+      <Image source={barConfig.assets.image} style={styles.backgroundImage} resizeMode="cover" />
 
-        {/* BOUTON 1 (Baillarock - IPA) */}
-        <TouchableOpacity 
-          style={[styles.button, { top: imageHeight * 0.33, left: width * 0.75 }]} 
-          onPress={() => Linking.openURL('https://www.instagram.com/brasserie_du_treize/')}>
-          <FontAwesome name="instagram" size={30} color="#fff" /> 
-        </TouchableOpacity>
+      {/* CONTENEUR DES BOUTONS SOUS L'IMAGE */}
+      <View style={styles.buttonContainer}>
+        {/* BRASSERIE DU XIII + BOUTON 1 */}
+        <View style={styles.buttonRow}>
+          <Text style={styles.buttonText}>Brasserie du XIII</Text>
+          <TouchableOpacity 
+            style={styles.button} 
+            onPress={() => Linking.openURL('https://www.instagram.com/brasserie_du_treize/')}>
+            <FontAwesome name="instagram" size={30} color="#fff" /> 
+          </TouchableOpacity>
+        </View>
 
-        {/* BOUTON 2 (Petroleuse - Ambrée/Blonde) */}
-        <TouchableOpacity 
-          style={[styles.button, { top: imageHeight * 0.40, left: width * 0.75 }]} 
-          onPress={() => Linking.openURL('https://www.instagram.com/piratesduclain/')}>
-          <FontAwesome name="instagram" size={30} color="#fff" /> 
-        </TouchableOpacity>
+        {/* BIÈRES DU CLAIN + BOUTON 2 */}
+        <View style={styles.buttonRow}>
+          <Text style={styles.buttonText}>Les Pirates du Clain</Text>
+          <TouchableOpacity 
+            style={styles.button} 
+            onPress={() => Linking.openURL('https://www.instagram.com/piratesduclain/')}>
+            <FontAwesome name="instagram" size={30} color="#fff" /> 
+          </TouchableOpacity>
+        </View>
       </View>
     </ScrollView>
   );
